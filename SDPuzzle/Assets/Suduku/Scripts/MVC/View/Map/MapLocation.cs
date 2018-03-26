fb8337a9c81922422ca6d519f898d25b14e3b8f9
zhangxinyu <zhangxinyu_a1120@163.com>
2018-03-26 11:05:13 +0800

    Map 地图

---
+++ SDPuzzle/Assets/Suduku/Scripts/MVC/View/Map/MapLocation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using strange.extensions.mediation.impl;
+
+public class MapLocation : View
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SDPuzzle/Assets/Suduku/Scripts/MVC/View/Map/MapWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using strange.extensions.mediation.impl;
+
+public class MapWorld : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
