128b043888fd27894a958de33fc0d65efb9b1c7d
chuining meng <mengchuining@fotoable.com>
2018-03-27 09:52:17 +0800

    Irosource Init

--- SDPuzzle/Assets/Suduku/Scripts/MVC/Controller/StartSdkCommand.cs
+++ SDPuzzle/Assets/Suduku/Scripts/MVC/Controller/StartSdkCommand.cs
@@ -8,6 +8,15 @@ public class StartSdkCommand : EventCommand {
 	public override void Execute()
 	{
 		//在这处理各个平台sdk初始化逻辑
+
+
+		#if UNITY_EDITOR 
+		#elif (UNITY_IPHONE || UNITY_IOS) 
+		IronSource.Agent.init ("6f8940fd");	
+		#elif UNITY_ANDROID
+		IronSource.Agent.init ("6f32bb75");
+		#endif
+		IronSource.Agent.validateIntegration();
 	}
 }
 
