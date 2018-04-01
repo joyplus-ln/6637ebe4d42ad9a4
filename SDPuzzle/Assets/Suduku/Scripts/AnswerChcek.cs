eb3c145411813cf65ca4e289c575ee6e87f577ce
lvjibin <lvjibin@fotoable.com>
2018-04-01 11:29:01 +0800

    add

---
+++ SDPuzzle/Assets/Suduku/Scripts/AnswerChcek.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerChcek  {
+
+    public static void CheckAnswerIsRight(List<Cell> cells)
+    {
+        //scells.FindAll(x=>x.)s
+    }
+}
